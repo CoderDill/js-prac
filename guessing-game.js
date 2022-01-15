4de967f88bacd245ee41c1bddc645339eac6cf13
CrappyDill <mddillon47@gmail.com>
2022-01-15 03:59:15 -0800

    guessing game

--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,21 @@
 function guessingGame() {
+    const target = Math.floor(Math.random() * 100)
+    let gameIsOver = false;
+    let guesses = 0;
 
+    return function (num) {
+        if (gameIsOver) return "The game is over, you already won!";
+        guesses++
+
+        if (target === num) {
+            gameIsOver = true;
+            const guess = guesses === 1 ? "guess" : "guesses";
+            return `You win! You found ${num} in ${guesses} ${guess}.` 
+        }
+
+        if(num < target) return `${num} is too low!`
+        if(num > target) return `${num} is too high!`
+    }
 }
 
 module.exports = { guessingGame };
